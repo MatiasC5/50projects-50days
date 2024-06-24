0f1a3142f3dbe90e21973bc35def3008ed92d5d7
MatiasC5 <cardosomatias1994@gmail.com>
2024-06-24 16:06:03 -0300

    changes

--- 1-Expanding_Cards/script.js
+++ 1-Expanding_Cards/script.js
@@ -2,6 +2,13 @@ const panel = document.querySelectorAll(".panel");
 
 panel.forEach((pan) => {
   pan.addEventListener("click", () => {
+    removeClass();
     pan.classList.add("active");
   });
 });
+
+function removeClass() {
+  panel.forEach((pan) => {
+    pan.classList.remove("active");
+  });
+}
