36d7b14fb97d4099da35a0a2eb08b167ebc533f9
MatiasC5 <cardosomatias1994@gmail.com>
2024-06-25 21:45:27 -0300

    .

--- 02-Progress_steps/script.js
+++ 02-Progress_steps/script.js
@@ -2,11 +2,47 @@ const prev = document.getElementById("prev");
 const next = document.getElementById("next");
 const progress = document.getElementById("progress");
 const circles = document.querySelectorAll(".circle");
-
 let currentActive = 1;
 
+// console.log(circles);
+
 circles.forEach((circle, index) => {
-  index < currentActive
-    ? circle.classList.add("active")
-    : circle.classList.remove("active");
+  if (index < currentActive) {
+    circle.classList.add("active");
+  } else {
+    circle.classList.remove("active");
+  }
+});
+
+function update() {
+  const actives = document.querySelectorAll(".active");
+
+  circles;
+  if (actives.length === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+
+  progress.style.width =
+    ((currentActive - 1) / (circles.length - 1)) * 100 + "%";
+}
+
+prev.addEventListener("click", () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  update();
+});
+
+next.addEventListener("click", () => {
+  currentActive++;
+
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+  update();
 });
