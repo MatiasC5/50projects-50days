7f51fded36d09ab2c419cff0b3a956f9ff82670a
MatiasC5 <cardosomatias1994@gmail.com>
2024-06-30 20:21:17 -0300

    Fixing the update function

--- 02-Progress_steps/script.js
+++ 02-Progress_steps/script.js
@@ -2,31 +2,32 @@ const prev = document.getElementById("prev");
 const next = document.getElementById("next");
 const progress = document.getElementById("progress");
 const circles = document.querySelectorAll(".circle");
+
 let currentActive = 1;
 
-// console.log(circles);
-
-circles.forEach((circle, index) => {
-  if (index < currentActive) {
-    circle.classList.add("active");
-  } else {
-    circle.classList.remove("active");
-  }
-});
-
 function update() {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
   const actives = document.querySelectorAll(".active");
 
-  circles;
+  progress.style.width =
+    ((currentActive - 1) / (circles.length - 1)) * 100 + "%";
+
+  if (actives.length <= 1) {
+    prev.disabled = true;
+  }
   if (actives.length === circles.length) {
     next.disabled = true;
   } else {
     prev.disabled = false;
     next.disabled = false;
   }
-
-  progress.style.width =
-    ((currentActive - 1) / (circles.length - 1)) * 100 + "%";
 }
 
 prev.addEventListener("click", () => {
